379cae58bbdf0a09f1191a837d795628fa929db0
AarioAi <AarioAi@gmail.com>
2016-03-09 20:28:59 +0800

    swift avatar UI

---
+++ Swift/1.0.0 UIKit Configurations.swift
@@ -0,0 +1,7 @@
+/**
+ * Setting status bar tint color
+ *  1. Add `View controller-based status bar appearance` in info.plist
+ *  2. UIApplication.sharedApplication().statusBarStyle = .LightContent 
+ */
+
+ 
